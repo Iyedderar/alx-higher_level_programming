2ad61bd3767b5d6b43f43fed6d58330a368834ab
Iyedderar <Iyedderar@yahoo.com>
2023-11-06 20:38:20 +0100

    fml

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,7 +2,7 @@
 def max_integer(my_list=[]):
     max = my_list[0]
     if len(my_list) == 0:
-        return (0)
+        return (None)
     else:
         for n in range(len(my_list)):
             if my_list[n] > max:
