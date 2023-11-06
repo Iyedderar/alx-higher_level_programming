a1b8425b48a04b90dfd31edb031a834d7adc8e1d
Iyedderar <Iyedderar@yahoo.com>
2023-11-06 20:51:11 +0100

    fml

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,54 @@
+#include "lists.h"
+
+/**
+ * strleng - Returns the length of a string.
+ * @s: The string to be measured.
+ *
+ * Return: The length of the string.
+ */
+int strleng(char *s)
+{
+	int len = 0;
+
+	if (*(s + len))
+	{
+		len++;
+		len += strleng(s + len);
+	}
+
+	return (len);
+}
+
+
+/**
+ * is_symmetric - Checks if a string is symteric.
+ * @s: The str to be checked.
+ * @len: The length.
+ *
+ * Return: If the number is symteric - 1.
+ *         If the number is not symteric- 0.
+ */
+int is_symmetric(char *s, int len)
+{
+	if (len == 0 || len == 1)
+		return (1);
+
+	if (*s == *(s + (len - 1)))
+		return (1 && is_symmetric(s + 1, len - 2));
+	else
+		return (0);
+}
+
+/**
+ * is_palindrome - Checks if a string is palindrome.
+ * @s: The string tobe checked.
+ *
+ * Return: If the integer is not palindrome - 0.
+ *         If the number is palindrome - 1.
+ */
+int is_palindrome(char *s)
+{
+	int len = strleng(s);
+
+	return (is_symmetric(s, len));
+}
